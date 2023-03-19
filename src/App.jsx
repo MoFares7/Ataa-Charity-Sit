413957e73900e1d7aa12292598d811ab4e28e581
MoFares7 <faresdabbas1@gmail.com>
2023-03-19 20:03:58 +0200

    add Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import About from "./components/Header/About";
-import Navbar from "./components/Header/Header";
-import HomePage from "./components/HomePage/HomePage";
-import LoginPage from "./components/LoginPage/LoginPage";
-import SignUpPage from "./components/SignUpPage/SignUpPage";
+import MainRoutes from "./Routes/Routs";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/loginPage" element={<LoginPage />} />
-          <Route path="/signupPage" element={<SignUpPage />} />
-
-        </Routes>
-
-
-      </BrowserRouter>
+      <MainRoutes />
     </div>
   );
 }
---
+++ src/Routes/Routs.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from "../components/Header/About";
+import Navbar from "../components/Header/Header";
+import HomePage from "../components/HomePage/HomePage";
+import LoginPage from "../components/LoginPage/LoginPage";
+import SignUpPage from "../components/SignUpPage/SignUpPage";
+
+export default function MainRoutes() {
+        return (
+                <div>
+                        <BrowserRouter>
+
+                                <Navbar />
+
+                                <Routes>
+                                        <Route path="/" element={<HomePage />} />
+                                        <Route path="/about" element={<About />} />
+                                        <Route path="/loginPage" element={<LoginPage />} />
+                                        <Route path="/signupPage" element={<SignUpPage />} />
+
+                                </Routes>
+
+                        </BrowserRouter>
+                </div>
+        )
+}
