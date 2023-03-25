229e77568329df402e820c6419fdf220c67b49ac
MoFares7 <faresdabbas1@gmail.com>
2023-03-25 21:46:15 +0200

    finish from header

---
+++ src/components/Body/HomePage/CardNews.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function CradNews() {
+        return (
+                <div className="Card-News">
+                        <img className="img-cardNews" src="https://images.unsplash.com/photo-1679189792474-6c2796dc78ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
+                        <p className="CardNews-Type">TECNOLOGY</p>
+                        <h5 className="CardNews-name">The News to apple</h5>
+
+                        <h5 className="CardNews-Descrption">Apple
+
+                                Apple
+                                https://www.apple.com
+                                Discover the innovative world of Apple and shop everything iPhone, iPad, Apple Watch, Mac, and Apple TV, plus explore accessories, entertainment, ...</h5>
+
+                        <button className="Button-Applay">Applay Now</button>
+                </div>
+        )
+}
--- src/components/Body/HomePage/HomePage.jsx
+++ src/components/Body/HomePage/HomePage.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import CardLeft from "./CardLeft";
+import CradNews from "./CardNews";
 import CardType from "./CardType";
-import DonationCampigen from "./CardType";
 import "./css/HomePage.css";
-
-
 
 export default function HomePage() {
         return (
                 <div className="main-back-home">
                         <div className="Show-FirstSection">
-                        <CardLeft />
-                        <div className="Show-CardType">
-                                <CardType />
-                                <CardType />
-                                <CardType />
+                                <CardLeft />
+                                <div className="Show-CardType">
+                                        <CardType />
+                                        <CardType />
+                                        <CardType />
+                                </div>
+
                         </div>
-                                   
+                        <div className="Show-News ">
+                                <h1>Some news</h1>
+                                <div className="Under-Point">
+
+                                </div>
+                                <div className="all-Card-News">
+                                        <CradNews />
+                                        <CradNews />
+                                        <CradNews />
+                                        <CradNews />
+                                </div>
                         </div>
-                   
                 </div>
+
         )
 }
