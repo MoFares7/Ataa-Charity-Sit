d9978bda156e1f9f0310d93abdbb893200e9ff74
MoFares7 <faresdabbas1@gmail.com>
2023-03-26 08:48:40 +0200

    finish from footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from "./components/Body/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import MainRoutes from "./Routes/Routs";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <HomePage />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import './Footer.css';
+import phone from '../../assets/phone.svg'
+import email from '../../assets/email.svg'
+import git from '../../assets/git.png'
+
+export default function Footer() {
+        return (
+                <div>
+                        <footer className="footer">
+                                <div className="container">
+                                        <div className="row">
+                                                <div className="col">
+                                                        <p>FM NEWS</p>
+                                                </div>
+                                                <div className="col">
+                                                        <p className="quicklinks">
+
+                                                                <p className="ph">
+                                                                        <img className="Phone-icon" src={email} />
+                                                                        <p className="phone-number">faresdabbas1@gmail.com</p>
+
+                                                                </p>
+                                                                <p className="ph">
+                                                                        <img className="Phone-icon" src={phone} />
+                                                                        <p className="phone-number">0943632624</p>
+                                                                </p>
+                                                                <p className="ph">
+                                                                        <img className="git-icon" src={git} />
+                                                                        <p className="phone-number">MoFares7</p>
+                                                                </p>
+
+                                                        </p>
+                                                </div>
+                                                <div className="col2">
+                                                        <h2>COMPANY</h2>
+                                                        <p>Apple</p>
+                                                        <p>Tesla</p>
+                                                        <p>BMV</p>
+                                                        <p>Volvo</p>
+                                                </div>
+                                                <div className="col2">
+                                                        <h2>SUPPORT</h2>
+                                                        <p>Apple</p>
+                                                        <p>Tesla</p>
+                                                        <p>BMV</p>
+                                                        <p>Volvo</p>
+                                                </div>
+                                                <div className="col2">
+                                                        <h2>COMMUNITY</h2>
+                                                        <p>Apple</p>
+                                                        <p>Tesla</p>
+                                                        <p>BMV</p>
+                                                        <p>Volvo</p>
+                                                </div>
+                                        </div>
+                                </div>
+                        </footer>
+                </div>
+        );
+}
+
